658b8808182841c7dd2ed61765aba78918e29717
MY FIRST PROJECT <mohamed.ongao@gmail.com>
2023-08-31 23:21:48 +0200

    initial commit

---
+++ HW12w/HW12w.cpp
@@ -0,0 +1,35 @@
+// HW12w.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\a";
+    cout << "******************************" << endl;
+    cout << "Name: Mohamed Abdu Omer." << endl;
+    cout << "Age : 44 years." << endl;
+    cout << "City: Vänersborg." << endl;
+    cout << "Country: Eritrea." << endl;
+    cout << "******************************" << endl;
+
+    cout << "********" << endl;
+    cout << "********" << endl;
+    cout << "********" << endl;
+    cout << "********" << endl << endl;
+
+    cout << "I love programming!" << endl;
+    cout << "I promise to be the best developer ever!" << endl;
+    cout << "I know it will take some time to practice,but I" << endl;
+    cout << "will achieve my goal!" << endl << endl;
+    cout << "Best Regards." << endl;
+    cout << "Mohamed Abdu." << endl << endl;
+
+    cout << "*     *" << endl;
+    cout << "*     *" << endl;
+    cout << "* *** *" << endl;
+    cout << "*     *" << endl;
+    cout << "*     *" << endl << endl;
+   
+}
